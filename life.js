15d9f055e6d2b3ccd3775a6c5a386947b7a6a34a
Merouane Atig <merouane.atig@gmail.com>
2015-07-21 07:44:59 +0200

    Initialize board from seed

--- life.js
+++ life.js
@@ -4,18 +4,29 @@
     this.seed = seed;
     this.height = seed.length;
     this.width = seed[0].length;
+    this.board = cloneArray(seed);
   };
 
   _.prototype = {
 
     toString: function() {
-      var rows = this.seed.map(function(row) {
+      var rows = this.board.map(function(row) {
         return row;
       });
       return rows.join('\n');
+    },
+
+    tick: function() {
     }
 
   };
+
+  function cloneArray(array) {
+    var rowsCopy = array.map(function(row) {
+      return row.slice();
+    });
+    return rowsCopy.slice();
+  }
 
 })();
 
@@ -26,5 +37,9 @@ var game = new Life([
     [0, 0, 1, 0, 0],
     [0, 0, 0, 0, 0]
 ]);
+
+console.log(game.toString());
+
+game.tick();
 
 console.log(game.toString());
